c23151cc14df49ed397e4600ad49b5881e7bda86
Xiangjun Yin <yin00048@algonquinlive.com>
2023-02-26 16:40:15 -0500

    add Game and Help menu and their menu items including start, shuffle, exit and about entries.

--- src/solitaire/gui/SuitStackView.java
+++ src/solitaire/gui/SuitStackView.java
@@ -8,7 +8,7 @@ public class SuitStackView extends StackPane{
 	
 	private static final int PADDING = 5;
 	private static final String BORDER_STYLE = "-fx-border-color: lightgray; -fx-border-width: 3; -fx-border-radius: 10.0;";
-	
+//	private static final String BORDER_STYLE = "-fx-border-color: blue; -fx-border-width: 3; -fx-border-radius: 10.0;";
 	public SuitStackView() {
 		setPadding(new Insets(PADDING));
 		setStyle(BORDER_STYLE);
