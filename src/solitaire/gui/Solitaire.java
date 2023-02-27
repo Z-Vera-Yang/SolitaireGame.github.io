46aee58cc4f145435465581e5763da8c8d6e9234
Xiangjun Yin <yin00048@algonquinlive.com>
2023-02-26 19:21:15 -0500

    Revert "add Game and Help menu and their menu items including start, shuffle, exit and about entries."
    This reverts commit c23151cc14df49ed397e4600ad49b5881e7bda86.

--- src/solitaire/gui/Solitaire.java
+++ src/solitaire/gui/Solitaire.java
@@ -1,23 +1,10 @@
 package solitaire.gui;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.ButtonBar.ButtonData;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import solitaire.card.Card.Suit;
-import solitaire.model.GameModel;
 import solitaire.model.SuitStackManager.SuitStack;
 import solitaire.model.WorkingStackManager.Workingstack;
 
@@ -27,7 +14,7 @@ public class Solitaire extends Application{
 	private static final int HEIGHT = 531;
 	private static final String TITLE ="Solitaire";
 	private static final String VERSION ="1.0";
-	private static final String GROUP = "CST8334 Group 14";
+	private static final String GROUP = "CST8334 Group 5";
 	
 	private DeckView deckView = new DeckView();
 	private DiscardPileView wasteView = new DiscardPileView();
@@ -42,78 +29,22 @@ public class Solitaire extends Application{
 	public void start(Stage arg0) throws Exception {
 		arg0.setTitle(TITLE+ " " + VERSION +"    " + GROUP);
 		
-		BorderPane root = new BorderPane();
-		GridPane gridPane = new GridPane();
+		GridPane root = new GridPane();
+		root.setStyle("-fx-background-image: url('file:./images/background.jpg')");
 		
-		gridPane.setStyle("-fx-background-image: url('file:./images/background.jpg')");
+		root.add(deckView, 0, 0);
+		root.add(wasteView, 1, 0);
+		for(SuitStack index : SuitStack.values()) {
+			suitStacks[index.ordinal()] = new SuitStackView();
+			root.add(suitStacks[index.ordinal()], 3+index.ordinal(), 0);
+		}
+		for(Workingstack index : Workingstack.values()) {
+			stacks[index.ordinal()] = new WorkingStackView(index);
+			root.add(stacks[index.ordinal()], index.ordinal(), 1);
+		}
 		
 		arg0.setResizable(false);
-		
-		MenuBar menuBar = new MenuBar();
-		
-		 // --- Menu Game
-        Menu menuGame = new Menu("Game");
-        MenuItem start = new MenuItem("Start");
-        start.setOnAction((ActionEvent t) -> {
-        	gridPane.add(deckView, 0, 0);
-    		gridPane.add(wasteView, 1, 0);
-    		for(SuitStack index : SuitStack.values()) {
-    			suitStacks[index.ordinal()] = new SuitStackView();
-    			gridPane.add(suitStacks[index.ordinal()], 3+index.ordinal(), 0);
-    		}
-    		for(Workingstack index : Workingstack.values()) {
-    			stacks[index.ordinal()] = new WorkingStackView(index);
-    			gridPane.add(stacks[index.ordinal()], index.ordinal(), 1);
-    		}
-    		 start.setDisable(true);
-        });
-        
-     // --- MenuItem Shuffle        
-        MenuItem shuffle = new MenuItem("Shuffle");
-        //GameModel.getInstance().reset();
-        shuffle.setOnAction((ActionEvent t) -> {
-        	GameModel.getInstance().reset();
-        });
-        
-     // --- MenuItem Exit   
-        MenuItem exit = new MenuItem("Exit");
-        exit.setOnAction((ActionEvent t) -> {
-            System.exit(0);
-        });
-        
-        menuGame.getItems().addAll(start, shuffle, exit);
-     // --- Menu Help
-        Menu menuHelp = new Menu("Help");
-     // --- MenuItem About    
-        MenuItem about = new MenuItem("About");
-		about.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				/*
-				 * Implement dialog to be prompted when players asks for
-				 * details.
-				 */
-				System.out.println("print Solitaire help information");
-				
-			      Dialog<String> dialog = new Dialog<String>();
-			      dialog.setTitle("Help");
-			      ButtonType type = new ButtonType("Ok", ButtonData.OK_DONE);
-			      dialog.setContentText("The Solitaire application implemented by Group 14 of CST8334_450 and directed by professor Moe Osman, thank you.");
-			      dialog.getDialogPane().getButtonTypes().add(type);
-			      dialog.showAndWait();
-			}
-		});
-		menuHelp.getItems().add(about);
-        
-        menuBar.getMenus().addAll(menuGame, menuHelp);
-        menuBar.setStyle("-fx-background-color: #ededed;");
-        //color options: Algonquin College green: #006341   blue:#4589bc, light green: #8be78b; light grey: #d3d3d3, lighter grey: #ededed
-        
-        root.setTop(menuBar);
-        root.setCenter(gridPane);
-        
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
-		arg0.setScene(scene);	
+		arg0.setScene(new Scene(root, WIDTH, HEIGHT));	
 		arg0.show();
 	}
 
