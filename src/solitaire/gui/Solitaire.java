4d25d307fe79e07918fa51113b34b28546a9f8ae
Z-Vera-Yang <algonquinlive.com>
2023-03-06 22:39:28 -0500

    change size of game window and game background

--- src/solitaire/gui/Solitaire.java
+++ src/solitaire/gui/Solitaire.java
@@ -21,8 +21,9 @@ import solitaire.model.WorkingStackManager.Workingstack;
 
 public class Solitaire extends Application{
 	
+	// set size of the game window and title 
 	private static final int WIDTH = 850;
-	private static final int HEIGHT = 531;
+	private static final int HEIGHT = 850;
 	private static final String TITLE ="Solitaire";
 	private static final String VERSION ="1.0";
 	private static final String GROUP = "CST8334 Group 14";
